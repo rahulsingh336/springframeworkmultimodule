75d8d09587f68bfa6072060b02bde1b0079704a0
Singh, Rahul <Rahul.Singh3@mastercard.com>
2018-02-08 15:13:16 +0530

    added start stop in bat files

--- Start-all.bat
+++ Start-all.bat
@@ -3,5 +3,5 @@ start java -jar  demotracing\target\tracing-zipkin-0.0.1-SNAPSHOT.jar
 start java -jar -Dspring.profiles.active=S1 -Dserver.port=9401 -Dspring.application.name=Service-1 zipkinservice\target\zipkinservice-0.0.1-SNAPSHOT.jar
 start java -jar -Dspring.profiles.active=S2 -Dserver.port=9402 -Dspring.application.name=Service-2 zipkinservice\target\zipkinservice-0.0.1-SNAPSHOT.jar 
 start java -jar -Dspring.profiles.active=S3 -Dserver.port=9403 -Dspring.application.name=Service-3 zipkinservice\target\zipkinservice-0.0.1-SNAPSHOT.jar 
-
-
+start java -jar -Dserver.port=8761  eurekaserver\target\eurekaserver-0.0.1-SNAPSHOT.jar 
+start java -jar -Dserver.port=8762 -Dspring.application.name=balance-service balanceservice\target\balanceservice-0.0.1-SNAPSHOT.jar 
--- Stop-all.bat
+++ Stop-all.bat
@@ -4,3 +4,5 @@ FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :9402') DO TaskK
 FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :9403') DO TaskKill.exe /PID %%P
 FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8080') DO TaskKill.exe /PID %%P
 FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8881') DO TaskKill.exe /PID %%P
+FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8761') DO TaskKill.exe /PID %%P
+FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8762') DO TaskKill.exe /PID %%P
